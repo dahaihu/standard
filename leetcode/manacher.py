a6d5b45b34f763a343aaed4d0902d7c1f5f856bc
hushichang <hushichang@stardust.ai>
2018-09-15 11:17:52 +0800

    提交两题leetcode

---
+++ leetcode/manacher.py
@@ -0,0 +1,37 @@
+class Solution:
+    def longestPalindrome(self, s):
+        # new_nums = '!#' + '#'.join(nums) + '#$'
+        nums = list(s)
+        new_nums = '!#' + '#'.join(nums) + '#$'
+        print('new_nums is {}'.format(new_nums))
+        mark = [0 for _ in range(len(new_nums))]
+        mxRight = 0
+        pos = 0
+        mx = 0
+        for i in range(1, len(new_nums)-1):
+            if i < mxRight:
+                # if mark[2*pos-i] < mxRight - i:
+                mark[i] = min(mark[2 * pos - i], mxRight - i)
+                    # while new_nums[i - mark[i]] == new_nums[i + mark[i]]:
+                    #     mark[i] += 1
+            # else:
+            while new_nums[i - mark[i] - 1] == new_nums[i + mark[i] + 1]:
+                mark[i] += 1
+            # mark[i] -= 1
+            print("i is {}, mark[i] is {}".format(new_nums[i], mark[i]))
+            if mark[i] + i > mxRight:
+                mxRight = mark[i] + 1
+                pos = i
+            if mx < mark[i]:
+                mx = mark[i]
+                res = new_nums[i-mark[i]:i+mark[i]+1]
+        print(mark)
+        return ''.join([ele for ele in res if ele != '#'])
+
+
+
+
+
+s = Solution()
+print(s.longestPalindrome('abba'))
+
---
+++ leetcode/nqueens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def solve(self, n):
+        nums = [0 for _ in range(n)]
+        self.dfs(nums, 0)
+        return self.count
+
+    def valid(self, nums, cur, ele):
+        for i in range(cur):
+            if nums[i] == ele or cur - i == abs(ele - nums[i]):
+                return False
+        return True
+
+    def dfs(self, nums, cur):
+        if cur == len(nums):
+            self.count += 1
+        else:
+            for i in range(len(nums)):
+                if self.valid(nums, cur, i):
+                    nums[cur] = i
+                    self.dfs(nums, cur+1)
+
+
+s = Solution()
+print(s.solve(8))
