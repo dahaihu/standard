385139d3f730493d69c7fca5c3b1b6cd2cc5e5b6
hushichang <hushichang@stardust.ai>
2018-09-20 10:35:03 +0800

    添加工作中的任务

---
+++ stardust/image_innatation.py
@@ -0,0 +1,82 @@
+from io import BytesIO
+import unicodedata
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageColor
+from PIL import ImageFont
+from urllib import request
+from itertools import cycle
+
+import io
+# from dashboard.commons.upload import Upload
+
+
+
+# 没有颜色的图片渲染
+class ImageAnnotation:
+    def __init__(self, url, annotation, name):
+        r = request.urlopen(url)
+        self.url = url
+        self.annotation = annotation
+        self.name = name
+        f = open('uploads/'+name+'.png', 'wb')
+        f.write(r.read())
+        f.close()
+        self.img = Image.open('uploads/'+name+'.png')
+        self.draw = ImageDraw.Draw(self.img, mode='RGBA')
+        self.colors = cycle([(229, 115, 115), (240, 98, 146), (186, 104, 200), (121, 134, 203), (79, 195, 247), (77, 208, 225), (77, 182, 172), (255, 241, 118), (255, 183, 77), (161, 136, 127)])
+
+
+    def draw_rectangle(self, anno, value):
+        color = next(self.colors)
+        points = [(point.get('x'), point.get('y')) for point in anno]
+        ind = 0
+        for i in range(1, 4):
+            if points[0][0] == points[i][0] or points[0][1] == points[i][1]:
+                continue
+            else:
+                ind = i
+                break
+        points[ind], points[2] = points[2], points[ind]
+        self.draw.polygon(points, fill=color+(50,), outline=color)
+        point_1, point_2 = points[0], points[2]
+        x = point_1[0] if point_1[0] < point_2[0] else point_2[0]
+        y = point_1[1] if point_1[1] < point_2[1] else point_2[1]
+        font = ImageFont.truetype('/home/hsc/下载/simsun.ttc', 18)
+        self.draw.text((x, y), value, font=font, fill=(0, 0, 0))
+
+    def export_polygon_rectangle(self):
+        """
+        包括多边形标注和矩形标注都可以用这个
+        :return:
+        """
+        draw = ImageDraw.Draw(self.img, mode='RGBA')
+        # data = self.data.get('point')
+        for item in self.annotation:
+            color = item.get('color')
+            points = item.get('points')
+            value = item.get('value')
+            # for value, anno in item.items():
+            #     self.draw_rectangle(anno.get('anno'), value)
+        #  得到图片的 binary 数据
+        output = BytesIO()
+        self.img.save('uploads/'+'hehe.png', format='PNG')
+        # upload = Upload()
+        # url = upload.qiniu_binary_upload('picture', self.name, output.getvalue())
+        # print(url)
+        # return url
+
+# image_url = 'https://qiniu-test.startask.net/20180822_alipalm2-thumb_down-fheart_0227_136_1537026634.jpg'
+# annotation = [{"color": "rgba(255,0,0,0.4)", "points": [{"x": 443.2837693270005, "y": 1082.4650746247853}, {"x": 691.5389083838324, "y": 1082.4650746247853}, {"x": 443.2837693270005, "y": 1499.039439800118}, {"x": 691.5389083838324, "y": 1499.039439800118}], "value": "\u6446\u59ff\u52bf\u7684\u624b"}, {"color": "rgba(255,0,0,0.4)", "points": [{"x": 644.1060184201081, "y": 832.8882910697675}, {"x": 951.5630921806885, "y": 832.8882910697675}, {"x": 644.1060184201081, "y": 1245.9801894978204}, {"x": 951.5630921806885, "y": 1245.9801894978204}], "value": "\u6446\u59ff\u52bf\u7684\u624b"}]
+# instance = ImageAnnotation(image_url, annotation, 'test')
+# instance.export_polygon_rectangle()
+
+info = {"status": 1, "answer": [{"<\u7236\u6807\u7b7e0>ppl": {"anno": [{"x": 381, "y": 283.5}, {"x": 483, "y": 283.5}, {"x": 381, "y": 424.5}, {"x": 483, "y": 424.5}], "attr": {"<\u7236\u6807\u7b7e0>ppl": {}}}, "<\u5b50\u6807\u7b7e00>h": {"anno": [{"x": 428, "y": 375.5}, {"x": 465, "y": 375.5}, {"x": 428, "y": 412.5}, {"x": 465, "y": 412.5}]}, "<\u5b50\u6807\u7b7e01>f": {"anno": [{"x": 390, "y": 295.5}, {"x": 441, "y": 295.5}, {"x": 390, "y": 384.05999999999995}, {"x": 441, "y": 384.05999999999995}]}}, {"<\u7236\u6807\u7b7e0>ppl": {"anno": [{"x": 223, "y": -8.5}, {"x": 304, "y": -8.5}, {"x": 223, "y": 121.06}, {"x": 304, "y": 121.06}], "attr": {"<\u7236\u6807\u7b7e0>ppl": {}}}, "<\u5b50\u6807\u7b7e00>h": {"anno": [{"x": 227, "y": -8.5}, {"x": 278.333984375, "y": -8.5}, {"x": 227, "y": 27.059999999999995}, {"x": 278.333984375, "y": 27.059999999999995}]}, "<\u5b50\u6807\u7b7e01>f": {"anno": [{"x": 258, "y": 42.5}, {"x": 292, "y": 42.5}, {"x": 258, "y": 116.06}, {"x": 292, "y": 116.06}]}}, {"<\u7236\u6807\u7b7e0>ppl": {"anno": [{"x": 433, "y": 21.5}, {"x": 519, "y": 21.5}, {"x": 433, "y": 130.06}, {"x": 519, "y": 130.06}], "attr": {"<\u7236\u6807\u7b7e0>ppl": {}}}, "<\u5b50\u6807\u7b7e00>h": {"anno": [{"x": 473, "y": 22.5}, {"x": 509, "y": 22.5}, {"x": 473, "y": 73.06}, {"x": 509, "y": 73.06}]}, "<\u5b50\u6807\u7b7e01>f": {"anno": [{"x": 440, "y": 73.5}, {"x": 479, "y": 73.5}, {"x": 440, "y": 131.06}, {"x": 479, "y": 131.06}]}}]}
+image_url = 'https://qiniu-test.startask.net/001F7A47E8B0_1524987015.backgroud_0_1537263745.jpg'
+for ele in info['answer']:
+    print(ele)
+    for key, value in ele.items():
+        print(key, value)
+annotaion = info.get('answer')
+instance = ImageAnnotation(image_url, annotaion, 'test')
+instance.export_polygon_rectangle()
---
+++ stardust/像素大小和是否在框外.py
@@ -0,0 +1,129 @@
+# anno = [{'color': 'rgba(208,2,27,0.4)', 'points': [{'x': 125, 'y': 188}, {'x': 154, 'y': 188}, {'x': 125, 'y': 212}, {'x': 154, 'y': 212}], 'value': '头'}, {'color': 'rgba(208,2,27,0.4)', 'points': [{'x': 111, 'y': 273}, {'x': 144, 'y': 273}, {'x': 111, 'y': 300}, {'x': 144, 'y': 300}], 'value': '头'}, {'color': 'rgba(208,2,27,0.4)', 'points': [{'x': 395, 'y': 422}, {'x': 420, 'y': 422}, {'x': 395, 'y': 446}, {'x': 420, 'y': 446}], 'value': '头'}, {'color': 'rgba(208,2,27,0.4)', 'points': [{'x': 478, 'y': 445}, {'x': 500, 'y': 445}, {'x': 478, 'y': 468}, {'x': 500, 'y': 468}], 'value': '头'}, {'color': 'rgba(139,87,42,0.4)', 'points': [{'x': 122, 'y': 166}, {'x': 217, 'y': 166}, {'x': 122, 'y': 238}, {'x': 217, 'y': 238}], 'value': '↓、下、南'}, {'color': 'rgba(126,211,33,0.4)', 'points': [{'x': 111, 'y': 257}, {'x': 225, 'y': 257}, {'x': 111, 'y': 313}, {'x': 225, 'y': 313}], 'value': '←、左、西'}, {'color': 'rgba(80,227,194,0.4)', 'points': [{'x': 363, 'y': 373}, {'x': 420, 'y': 373}, {'x': 363, 'y': 448}, {'x': 420, 'y': 448}], 'value': '↘、右下、东南'}, {'color': 'rgba(208,2,27,0.4)', 'points': [{'x': 330, 'y': 441}, {'x': 351, 'y': 441}, {'x': 330, 'y': 460}, {'x': 351, 'y': 460}], 'value': '头'}, {'color': 'rgba(80,227,194,0.4)', 'points': [{'x': 313, 'y': 421}, {'x': 351, 'y': 421}, {'x': 313, 'y': 462}, {'x': 351, 'y': 462}], 'value': '↘、右下、东南'}, {'color': 'rgba(189,16,224,0.4)', 'points': [{'x': 460, 'y': 420}, {'x': 500, 'y': 420}, {'x': 460, 'y': 469}, {'x': 500, 'y': 469}], 'value': '↖、左上、西北'}]
+#
+# a = {"status": 2, "answer": [{"color": "rgba(255,0,0,0.4)", "points": [{"x": 218.04402087587914, "y": 490.00907646925344}, {"x": 365.96709779895616, "y": 490.00907646925344}, {"x": 218.04402087587914, "y": 733.3167687769458}, {"x": 365.96709779895616, "y": 733.3167687769458}], "value": "\u6446\u59ff\u52bf\u7684\u624b"}, {"color": "rgba(255,0,0,0.4)", "points": [{"x": 356.5055593374176, "y": 484.62446108463814}, {"x": 529.0440208758793, "y": 484.62446108463814}, {"x": 356.5055593374176, "y": 782.547538007715}, {"x": 529.0440208758793, "y": 782.547538007715}], "value": "\u6446\u59ff\u52bf\u7684\u624b"}]}
+# for ele in a.get('answer'):
+#     print(ele)
+
+import csv
+import re
+import requests
+from io import BytesIO
+import unicodedata
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageColor
+from PIL import ImageFont
+from urllib import request
+from itertools import cycle
+
+d = {'南':0, '西':1, '北':2, '东':3, '西南':4, '东南':5, '东北':6,'西北':7,'头':8, '脚':9}
+def func():
+    path = '/home/hsc/下载/50试标_501_1537357865.csv'
+    with open(path, 'r', newline='') as file:
+        csvreader = csv.reader(file)
+        c = 0
+        for ind, line in enumerate(csvreader):
+            if ind == 0:
+                continue
+            # # 获取图像的url
+            url = eval(line[1]).get('image_url')
+            # resp = requests.get(url)
+            # # # 获取图像的内容
+            # # img = resp.content
+            # print('url is ', url)
+            # f = open(url.split('/')[-1], 'wb')
+            # f.write(resp.content)
+            img = Image.open(url.split('/')[-1])
+            size = img.size
+            print('size is ', size)
+            # break
+            # 获取所有的点
+            for point_list in eval(line[-1]):
+                # 获取画一个对象的坐标，和值
+                points = [[ele.get('x'), ele.get('y')] for ele in point_list.get('points')]
+                value = point_list.get('value')
+                if '、' in value:
+                    z = d[value.split('、')[-1]]
+                else:
+                    z = d[value[-1]]
+                if not isinstance(z, int):
+                    print('invalid z is {}'.format(z))
+                    return
+                # 调整四个点的顺序，可以顺序连一个点
+                ind = 0
+                for i in range(1, 4):
+                    if points[0][0] == points[i][0] or points[0][1] == points[i][1]:
+                        continue
+                    else:
+                        ind = i
+                        break
+                points[ind], points[2] = points[2], points[ind]
+                print(points)
+                for point in points:
+                    point[0] = size[0] if point[0] > size[0] else point[0]
+                    point[1] = size[1] if point[1] > size[1] else point[1]
+                final_points = [[min(points[0][0], points[2][0]), min(points[0][1], points[2][1])], [max(points[0][0], points[2][0]), max(points[0][1], points[2][1])]]
+                print('final_points', final_points)
+                kuan = abs(final_points[0][0] - final_points[1][0])
+                gao = abs(final_points[0][1] - final_points[1][1])
+                # kuan = abs(points[0][0] - points[2][0])
+                # gao = abs(points[0][1] - points[2][1])
+                if kuan < 15 or gao < 15:
+                    if kuan < 15:
+                        final_points[0][0] -= 15 - kuan
+                    elif gao < 15:
+                        final_points[0][1] -= 15 - gao
+                    if final_points[0][0] < 0 and not final_points[0][1] < 0:
+                        print(final_points)
+                        final_points = [[0, final_points[0][1]], [final_points[1][0]+abs(final_points[0][0]), final_points[1][1]]]
+                        print(final_points)
+                        print(1)
+                        return
+                    elif final_points[0][1] < 0 and not final_points[0][0] > 0:
+                        print(final_points)
+                        final_points = [[final_points[0][0], 0], [final_points[1][0], final_points[1][1] + abs(final_points[0][1])]]
+                        print(final_points)
+                        print(2)
+                        return
+                    elif final_points[0][0] < 0 and final_points[0][1] < 0:
+                        print(final_points)
+                        final_points = [[0, 0], [15, 15]]
+                        print(final_points)
+                        print(3)
+                        return
+                kuan = abs(final_points[0][0] - final_points[1][0])
+                gao = abs(final_points[0][1] - final_points[1][1])
+                # print(url.split('/')[-1].split('.')[0])
+                print(re.sub(r'(?<=_0)_\d{10}', '', '.'.join(url.split('/')[-1].split('.')[:-1])) + '.txt')
+                with open('result/'+re.sub(r'(?<=_0)_\d{10}', '', '.'.join(url.split('/')[-1].split('.')[:-1])) + '.txt', newline='', mode='a') as file:
+                    file.write(str(z) + '\t')
+                    file.write(str((final_points[0][0] + final_points[1][0])/2/size[0]) + '\t')
+                    file.write(str((final_points[0][1] + final_points[1][1])/2/size[1]) + '\t')
+                    file.write(str((kuan)/size[0]) + '\t')
+                    file.write(str((gao)/size[1]) + '\t')
+                    file.write('\n')
+
+
+
+
+
+
+
+
+
+                    #     print(True)
+                    # else:
+                    #     with open('error.csv', 'a', newline='') as file:
+                    #         csvwriter = csv.writer(file)
+                    #         csvwriter.writerow(line)
+                    #     c += 1
+                    #     print(point)
+                    #     print(False)
+                        # return
+
+
+
+            # print(point_list)
+        print('error count is ', c)
+
+func()
