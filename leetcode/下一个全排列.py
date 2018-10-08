bab567c1419c3830b5ccb8ab4643f61c95ab25c4
hudahai <1431793998@qq.com>
2018-10-08 21:11:27 +0800

    '添加下一个全排列的注释''

--- leetcode/下一个全排列.py
+++ leetcode/下一个全排列.py
@@ -12,13 +12,18 @@
 
 
 class Solution:
+    """
+    总结起来就是两步
+    1. 找到需要调换元素的索引，进行调换
+    2. 调换之后，后面得元素要逆序排列一下，生成交换后的最小值
+    """
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         tmp = 0
-        # 逆序遍历， 只到索引为1的
+        # 逆序遍历， 只到索引为1的, 找到一个索引，这个索引i指向的元素值大于索引i-1所指向的元素值
         for i in range(len(nums) - 1, 0, -1):
             print("i is {}".format(i))
             if nums[i] > nums[i - 1]:
@@ -26,17 +31,20 @@ class Solution:
                 print('i is {}, tmp is {}'.format(i, tmp))
                 break
         else:
+            # 没有这个索引，就说明nums是根据大小逆序排列的，然后逆序回来即可
             left, right = 0, len(nums) - 1
             while left < right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
                 right -= 1
             return
+        # 找到索引i之后的大于这个值得最小值，然后进行替换
         for i in range(len(nums) - 1, tmp - 1, -1):
             print("i is {}".format(i))
             if nums[i] > nums[tmp - 1]:
                 nums[i], nums[tmp - 1] = nums[tmp - 1], nums[i]
                 break
+        # 替换完成之后，i之后的元素逆序排列一下，因为i之后的元素是从大到小排列的
         right = len(nums) - 1
         while tmp < right:
             nums[tmp], nums[right] = nums[right], nums[tmp]
