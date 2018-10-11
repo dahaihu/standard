fc765a38fa62dc653cfd7d82ca9be34ae2989404
hudahai <1431793998@qq.com>
2018-10-11 14:00:49 +0800

    '买股票的最佳时机'

--- leetcode/买股票的最佳时机.py
+++ leetcode/买股票的最佳时机.py
@@ -16,6 +16,27 @@ class Solution:
         for k in range(1, 3):
             tmp_min = prices[0]
             for i in range(1, len(prices)):
+                # 感觉这样分开算很是惨绝人寰的难易理解
                 tmp_min = min(tmp_min, prices[i] - dp[k - 1][i - 1])
                 dp[k][i] = max(dp[k][i - 1], prices[i] - tmp_min)
         return dp[-1][-1]
+
+
+# 通过一次的来理解多次的计算方式
+def once(nums):
+    # dp = [0]
+    # tmp_min = nums[0]
+    # for i in range(1, len(nums)):
+    #     # 要透过现象看本质
+    #     tmp_min = min(tmp_min, nums[i])
+    #     dp.append(nums[i] - tmp_min)
+    # return max(dp)
+    dp = [0 for _ in range(len(nums))]
+    tmp_min = nums[0]
+    for i in range(1, len(nums)):
+        tmp_min = min(tmp_min, nums[i])
+        dp[i] = max(nums[i] - tmp_min, dp[i - 1])
+    return dp[-1]
+
+nums = [7, 1, 5, 3, 6, 4]
+print(once(nums))
