b3166b349575be298bf29169d5cbd31052c15770
hudahai <1431793998@qq.com>
2018-10-14 09:02:38 +0800

    '再来一遍第k个排列'

--- leetcode/第k个排列.py
+++ leetcode/第k个排列.py
@@ -5,19 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        mark = [i for i in range(1, n + 1)]
+        mark = [i for i in range(1, n+1)]
         table = [1 for _ in range(n)]
         for i in range(1, n):
-            table[i] = i * table[i - 1]
-        print(table)
+            table[i] = i * table[i-1]
         res = ''
         k -= 1
-        while n > 1:
-            print('k is {}, table[n-1] is {}'.format(k, table[n - 1]))
-            ind, k = divmod(k, table[n - 1])
+        # while n > 0:
+        while mark:
+            ind, k = divmod(k, table[n-1])
             res += str(mark.pop(ind))
             n -= 1
-        return res + str(mark[0])
+        return res
 
 
 s = Solution()
