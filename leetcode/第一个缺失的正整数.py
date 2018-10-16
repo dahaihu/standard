ad7bf64fd364c0a5fc13acc795b03fc551dc166f
hudahai <1431793998@qq.com>
2018-10-16 08:42:20 +0800

    '第二次重写'

--- leetcode/第一个缺失的正整数.py
+++ leetcode/第一个缺失的正整数.py
@@ -10,10 +10,8 @@ class Solution:
         for i in range(n):
             if nums[i] < 0 or nums[i] >= n:
                 nums[i] = 0
-
         for i in range(n):
             nums[nums[i] % n] += n
-
         for i in range(1, n):
             if nums[i] < n:
                 return i
