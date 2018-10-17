79f8ee7924902a2d5941a4c776b097e2d0a12198
hudahai <1431793998@qq.com>
2018-10-17 19:03:23 +0800

    '众数'

---
+++ leetcode/环2.py
@@ -0,0 +1,38 @@
+"""
+142. 环形链表 II
+题目描述提示帮助提交记录社区讨论阅读解答
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+说明：不允许修改给定的链表。
+
+进阶：
+你是否可以不用额外空间解决此题？
+
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast is slow:
+                break
+        else:
+            return
+        slow = head
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
