832135d1a47cfac6905fab45a106a75fb203ee5b
胡世昌 <hushichang@hushichangdeMacBook-Pro.local>
2019-01-06 09:59:01 +0800

    添加元类的继续学习

--- studing/metaclass.py
+++ studing/metaclass.py
@@ -1,7 +1,20 @@
+def say_hello(self):
+    print("hello 啊")
+
+
 class A(type):
     # 需要注意的是元类的new方法的第一个参数是什么
+    # 似乎只能通过new方法来修改attrs
+    # 在init方法里面添加attrs是没有用的
     def __new__(mcs, name, bases, attrs):
-        pass
+        print("new attrs are ", attrs)
+        print('name is ', name)
+        attrs['say_hello'] = say_hello
+        return super().__new__(mcs, name, bases, attrs)
+
+    def __init__(cls, name, bases, attrs):
+        print("__init__ attrs are ", attrs)
+        super().__init__(name, bases, attrs)
 
 
 class Singleton(type):
@@ -10,15 +23,24 @@ class Singleton(type):
         super().__init__(name, bases, attrs)
 
     def __call__(cls, *args, **kwargs):
+        print("call is being called")
         if not cls._instance:
             cls._instance = super().__call__(*args, **kwargs)
+        print("call has been called")
         return cls._instance
 
 
 class B(metaclass=Singleton):
-    pass
+    def __init__(self):
+        print("init is being called")
 
 
-b1 = B()
-b2 = B()
-print(b1 is b2)
+def __init__(self):
+    self.message = "Hello world"
+
+
+attrs = {"__init__": __init__}
+
+Hello = A("Hello", (object,), attrs)
+h = Hello()
+h.say_hello()
