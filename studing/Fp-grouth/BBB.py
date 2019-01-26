543b2305c26247a46470fc48c956bd34fe4d8de9
shichang.hu <shichang.hu@hobot.cc>
2019-01-26 18:42:15 +0800

    似乎完成的很成功

---
+++ studing/Fp-grouth/BBB.py
@@ -0,0 +1,204 @@
+import time
+
+
+class SortedTree:
+    def __init__(self, node, prefix, result, min_sup, dataset=None):
+        self.result = result
+        self.node = node
+        self.prefix = prefix
+        self.support = 0
+        self.min_sup = min_sup
+        self.children = []
+        self.dataset = [] if not dataset else dataset
+
+    def canLinked(self):
+        return len(self.dataset) >= self.min_sup
+
+    # 给一个节点添加前缀，那么就是该节点的前缀，加上该节点的值
+    def addChild(self, node):
+        self.children.append(SortedTree(node, [node] + self.prefix, self.result, self.min_sup))
+
+    def addTransaction(self, transaction):
+        self.dataset.append(transaction)
+
+    def index(self, mark):
+        return mark[self.node]
+
+    def valid_candidate(self, candidate, fpset):
+        candidate = set(candidate)
+        print("candidate is {}".format(candidate))
+        print("fpset is {}".format(fpset))
+        for element in candidate:
+            if (candidate - {element}) not in fpset:
+                return False
+        return True
+
+    # 这一部分的计算通过广度优先的策略
+    def linking_width_first(self, mark, fqsets, pre=True):
+        print("prefix is {}".format(self.prefix))
+        print("self.children is {}".format([child.node for child in self.children]))
+        tmp = [(1 << child.index(mark)) for child in self.children]
+        print("tmp is {}".format(tmp))
+        """
+        第一步，给每个孩子节点分数据
+        """
+        # 划分数据集
+        for transaction in self.dataset:
+            for ind, ttt in enumerate(tmp):
+                if transaction & ttt == ttt:
+                    self.children[ind].addTransaction(transaction)
+                    # break
+
+        """
+        第二步，筛选不是频繁项集的子节点
+        
+        这个不就是相当于已经过滤了一遍了吗？
+        那你个煞笔的第三步是用来干什么的？
+        用来浪费时间的吗？
+        傻屌东西
+        
+        这样的话，速度可以提升多少呢？
+        
+        可以一倍的吗？
+        
+        """
+        cur = 0
+        while cur < len(self.children):
+            if len(self.children[cur].dataset) < self.min_sup:
+                self.children.pop(cur)
+            else:
+                cur += 1
+        """
+        第三步，给孩子节点分可能的子节点
+        """
+        for i in range(len(self.children) - 1):
+            for j in range(i + 1, len(self.children)):
+                if pre or self.valid_candidate([self.children[j].node] + self.children[i].prefix, fqsets):
+                    self.children[i].addChild(self.children[j].node)
+
+        self.support = len(self.dataset)
+        del self.dataset
+        # """
+        # 第三步，对孩子节点进行深度优先的挖掘
+        # 这一步是不是应该在外部做，这样可以按照广度优先的方式来计算，从而过滤掉没必要的项集
+        # """
+        # for child in self.children:
+        #     if child.canLinked():
+        #         self.result.append([child.node] + self.prefix)
+        #         child.linking_width_first(mark)
+
+
+class Best:
+    """
+    dataset: 原始的数据集
+    mark: 单项集 和 支持度， 以后的每个事务记录的排序都是根据这个进行排序
+    """
+
+    def __init__(self, minSup, dataset=None):
+        self.dataset = dataset if dataset else self.loadDataSet()
+        self.minSup = minSup
+        self.mark = {}
+        self.res = None
+
+    def loadDataSet(self):
+        return [['1', '3', '4'], ['2', '3', '5'], ['1', '2', '3', '5'], ['2', '5']]
+        # return [{'A', 'B', 'C', 'D'}, {'C', 'E'}, {'C', 'D'}, {'A', 'C', 'D'}, {'C', 'D', 'E'}]
+
+    def scanDataset(self):
+        for items in self.dataset:
+            for item in items:
+                self.mark[item] = self.mark.get(item, 0) + 1
+        for item, support in list(self.mark.items()):
+            # 感觉自己就像个傻逼一样，无药可救的很
+            if support < self.minSup:
+                del self.mark[item]
+        # 从小到大排列的
+        self.res = sorted(self.mark, key=lambda x: self.mark[x], reverse=True)
+        self.fk_1 = self.mark
+        print("fk_1 is {}".format(self.fk_1))
+        self.mark = {node: (len(self.res) - ind - 1) for ind, node in enumerate(self.res)}
+
+    # 这个地方fk1，看情况应该是从大到小排序就可以正确的编码了
+    # 因为值是从小到大进行排列
+    # 从后又从前到后的判断
+    # 前面的移动的位次是最多的
+    def encode(self, fk1, data):
+        print("data is {}".format(data))
+        print("fk1 is {}".format(fk1))
+        encodedAffair = []
+        for a in data:
+            tmp = 0
+            for ele in fk1:
+                tmp = (tmp << 1) + 1 if ele in a else tmp << 1
+            # 实际上的运算还是得用这个
+            encodedAffair.append(tmp)
+            # 这个可以用来做视觉上的展示
+            # res.append(bin(tmp))
+        return encodedAffair
+
+    def encode_transaction(self, fk1, data):
+        tmp = 0
+        for ele in fk1:
+            tmp = (tmp << 1) + 1 if ele in data else tmp << 1
+        return tmp
+
+    def main(self):
+        result = []
+        self.scanDataset()
+        # print("fk_1 is {}".format(self.fk_1))
+        # encode传入的应该是按照频繁项集从小到大排序的数组
+        data = self.encode(self.res, self.dataset)
+        # # 展示编码结果
+        # return data
+        root = SortedTree('', [], result, self.minSup, data)
+        # print("self.res is {}".format(self.res))
+        # print("self.mark is {}".format(self.mark))
+        res = []
+        for node in self.res:
+            root.addChild(node)
+        # if root.canLinked():
+        #     root.linking_width_first(self.mark)
+        res.append(root)
+        count = 0
+        while res:
+            count += 1
+            tmp = []
+            result.append(set())
+            for node in res:
+                if count > 2:
+                    node.linking_width_first(self.mark, result[-2],
+                                             pre=False)
+                else:
+                    node.linking_width_first(self.mark, set(), pre=True)
+                for child in node.children:
+                    tmp.append(child)
+                    result[-1].add(frozenset(child.prefix))
+            res = tmp
+        print(result)
+        print(len(result))
+
+
+def loadDataset(path):
+    dataset = []
+    with open(path, 'r') as file:
+        for line in file:
+            print(line.strip().split(' '))
+            dataset.append(line.strip().split(' '))
+    global minSup
+    minSup = len(dataset) * 0.2
+    return minSup, dataset
+
+
+if __name__ == '__main__':
+    # loadDataset(r'C:\Users\shichang.hu\Desktop\mushroom.dat.txt')
+    start = time.time()
+    # minSup, dataset = loadDataset(r'C:\Users\shichang.hu\Desktop\mushroom.dat.txt')
+    dataset = [{'A', 'B', 'C', 'D'}, {'C', 'E'}, {'C', 'D'}, {'A', 'C', 'D'}, {'C', 'D', 'E'}]
+    minSup = 2
+    b = Best(minSup, dataset)
+    # b = Best(0.2, dataset=loadDataset(r'/Users/hushichang/mushroom.dat.txt'))
+    # b = Best(0.2, dataset=loadDataset(r'/Users/hushichang/mushroom.dat.txt'))
+    # b = Best(2)
+    # b = Best(0.2)
+    res = b.main()
+    print('cost time is {}'.format(time.time() - start))
