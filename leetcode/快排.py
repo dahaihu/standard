3a9eada95e3c542325ebd39db3a1d4f47e3e3130
hushichang <hushichang@zhihu.com>
2019-10-19 11:45:05 +0800

    添加快排说明、修改函数命名方式

--- leetcode/快排.py
+++ leetcode/快排.py
@@ -8,6 +8,10 @@ def func(nums, start, end):
             left += 1
         while nums[right] > nums[start]:
             right -= 1
+        # 等于的时候也得退出
+        # exp: [5, 2, 4, 4, 3, 3]
+        # 此时 left = right = 5
+        # 不退出的话，left得继续减1，从而造成mid =  4, 而5没有移动到最右边最大值的位置
         if left >= right:
             break
         nums[left], nums[right] = nums[right], nums[left]
@@ -17,16 +21,16 @@ def func(nums, start, end):
     return right
 
 
-def quickSort(nums, start, end):
+def quick_sort(nums, start, end):
     if start < end:
         mid = func(nums, start, end)
-        quickSort(nums, start, mid - 1)
-        quickSort(nums, mid + 1, end)
+        quick_sort(nums, start, mid - 1)
+        quick_sort(nums, mid + 1, end)
 
 
 if __name__ == '__main__':
     nums = [random.randint(1, 10) for _ in range(10)]
     print('original nums is ', nums)
     random.shuffle(nums)
-    quickSort(nums, 0, len(nums) - 1)
+    quick_sort(nums, 0, len(nums) - 1)
     print('sorted nums is ', nums)
